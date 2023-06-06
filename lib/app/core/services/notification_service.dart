485ec54328c5926403b221e7dc52db88b4297988
Paulo Ricardo <pauloricardomsop@gmail.com>
2023-06-06 13:51:01 -0300

    update version and remove notification

--- lib/app/core/services/notification_service.dart
+++ lib/app/core/services/notification_service.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:svr/app/core/ad/ad_controller.dart';
 import 'package:svr/app/core/services/foreground_service.dart';
 import 'package:svr/app/core/utils/global_resource.dart';
@@ -56,65 +56,65 @@ class NotificationService {
       );
 }
 
-late AndroidNotificationChannel channel;
-late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+// late AndroidNotificationChannel channel;
+// late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
-String? urlToShow;
+// String? urlToShow;
 
-Future<void> initFirebaseMessaging() async {
-  FirebaseMessaging.instance.subscribeToTopic('scheduled_notification');
-  FirebaseMessaging.onMessage.listen(showFlutterNotification);
-  FirebaseMessaging.onMessageOpenedApp.listen((message) {
-    ForegroundService.showForegroundBack = true;
-    urlToShow = message.data['url'];
-  });
-  setupFlutterNotifications();
-}
+// Future<void> initFirebaseMessaging() async {
+//   FirebaseMessaging.instance.subscribeToTopic('scheduled_notification');
+//   FirebaseMessaging.onMessage.listen(showFlutterNotification);
+//   FirebaseMessaging.onMessageOpenedApp.listen((message) {
+//     ForegroundService.showForegroundBack = true;
+//     urlToShow = message.data['url'];
+//   });
+//   setupFlutterNotifications();
+// }
 
-Future<void> setupFlutterNotifications() async {
-  channel = const AndroidNotificationChannel(
-    'high_importance_channel',
-    'High Importance Notifications',
-    description: 'This channel is used for important notifications.',
-    importance: Importance.high,
-  );
+// Future<void> setupFlutterNotifications() async {
+//   channel = const AndroidNotificationChannel(
+//     'high_importance_channel',
+//     'High Importance Notifications',
+//     description: 'This channel is used for important notifications.',
+//     importance: Importance.high,
+//   );
 
-  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//   flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
+//   await flutterLocalNotificationsPlugin
+//       .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+//       ?.createNotificationChannel(channel);
 
-  flutterLocalNotificationsPlugin.initialize(
-      const InitializationSettings(android: AndroidInitializationSettings('ic_notification')),
-      onSelectNotification: selectNotificationInForeground);
-}
+//   flutterLocalNotificationsPlugin.initialize(
+//       const InitializationSettings(android: AndroidInitializationSettings('ic_notification')),
+//       onSelectNotification: selectNotificationInForeground);
+// }
 
-void showFlutterNotification(RemoteMessage message) {
-  RemoteNotification? notification = message.notification;
-  AndroidNotification? android = message.notification?.android;
-  if (notification != null && android != null && !kIsWeb) {
-    flutterLocalNotificationsPlugin.show(
-      notification.hashCode,
-      notification.title,
-      notification.body,
-      NotificationDetails(
-        android: AndroidNotificationDetails(
-          channel.id,
-          channel.name,
-          channelDescription: channel.description,
-          icon: 'ic_notification',
-        ),
-      ),
-      payload: message.data['url'],
-    );
-  }
-}
+// void showFlutterNotification(RemoteMessage message) {
+//   RemoteNotification? notification = message.notification;
+//   AndroidNotification? android = message.notification?.android;
+//   if (notification != null && android != null && !kIsWeb) {
+//     flutterLocalNotificationsPlugin.show(
+//       notification.hashCode,
+//       notification.title,
+//       notification.body,
+//       NotificationDetails(
+//         android: AndroidNotificationDetails(
+//           channel.id,
+//           channel.name,
+//           channelDescription: channel.description,
+//           icon: 'ic_notification',
+//         ),
+//       ),
+//       payload: message.data['url'],
+//     );
+//   }
+// }
 
-void handleInitialMessage(RemoteMessage? message) {
-  if (message == null) return;
-  execUrl(message.data['url']);
-}
+// void handleInitialMessage(RemoteMessage? message) {
+//   if (message == null) return;
+//   execUrl(message.data['url']);
+// }
 
-Future selectNotificationInForeground(String? payload) async =>
-    AdController.showInsterticialAdNotification(AdController.adConfig.intersticial.id, payload!);
+// Future selectNotificationInForeground(String? payload) async =>
+//     AdController.showInsterticialAdNotification(AdController.adConfig.intersticial.id, payload!);
