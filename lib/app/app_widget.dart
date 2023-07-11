def8585dbc1df76f8f339fd3222ea8f21c9761d1
Paulo Ricardo <pauloricardomsop@gmail.com>
2023-07-11 14:36:34 -0300

    ajuste

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'dart:ui';
 
 // import 'package:facebook_app_events/facebook_app_events.dart';
-import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
