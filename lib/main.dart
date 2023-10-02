803ab72ce46174ef8d3940a462da8c0ca461d686
Ernesto Moraes <joseernestosantosmoraes@gmail.com>
2023-10-02 08:35:17 -0300

    retirando mudanca de cor da stautsbar da main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
 import 'dart:ui';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:svr/app/app_widget.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor:
-        Colors.blue, // Change this color to your desired status bar color
-    statusBarIconBrightness: Brightness
-        .light, // Change this to Brightness.dark if your status bar content is light-colored
-  ));
-
   FlutterError.onError = (errorDetails) {
     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
   };
