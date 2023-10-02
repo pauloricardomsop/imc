d43b903ef7769afeeeddac727871b5514586fcca
Paulo Ricardo <pauloricardomsop@gmail.com>
2023-10-02 17:02:00 -0300

    ajuste

--- lib/app/core/services/ad_manager_service.dart
+++ lib/app/core/services/ad_manager_service.dart
@@ -1,10 +1,10 @@
 import 'package:ad_manager/ad_manager.dart';
-import 'package:svr/app/app_controller.dart';
-import 'package:svr/app/core/models/service_model.dart';
-import 'package:svr/app/modules/splash/ui/splash_page.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:log_manager/log_manager.dart';
+import 'package:svr/app/app_controller.dart';
+import 'package:svr/app/core/models/service_model.dart';
+import 'package:svr/app/modules/splash/ui/splash_page.dart';
 
 class AdManagerService implements Service {
   @override
@@ -23,7 +23,7 @@ class AdManagerService implements Service {
   static final Map<String, dynamic> config = {
     "activeAll": true,
     "appOpen": {
-      "active": true,
+      "active": false,
       "idIOS": "ca-app-pub-8419654969814276/7184142484",
       "idAndroid": "ca-app-pub-8419654969814276/4927579039",
     },
@@ -33,12 +33,12 @@ class AdManagerService implements Service {
       "idAndroid": "ca-app-pub-8419654969814276/6666047983",
     },
     "intersticial": {
-      "active": true,
+      "active": false,
       "idIOS": "ca-app-pub-8419654969814276/3520229645",
       "idAndroid": "ca-app-pub-8419654969814276/6721888307",
     },
     "rewarded": {
-      "active": true,
+      "active": false,
       "idIOS": "ca-app-pub-8419654969814276/5978872021",
       "idAndroid": "ca-app-pub-8419654969814276/5856363345",
     }
