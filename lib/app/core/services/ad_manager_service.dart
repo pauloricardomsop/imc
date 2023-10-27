a232977103ade693cbd848d166ea95ac06b33f63
Ernesto Moraes <joseernestosantosmoraes@gmail.com>
2023-10-27 20:57:48 -0300

    update version 5.3.2 - sem consulta cpf

--- lib/app/core/services/ad_manager_service.dart
+++ lib/app/core/services/ad_manager_service.dart
@@ -23,7 +23,7 @@ class AdManagerService implements Service {
   static final Map<String, dynamic> config = {
     "activeAll": true,
     "appOpen": {
-      "active": false,
+      "active": true,
       "idIOS": "ca-app-pub-8419654969814276/5311849118",
       "idsAndroid": [
         "ca-app-pub-8419654969814276/6331069701",
@@ -32,12 +32,12 @@ class AdManagerService implements Service {
       ],
     },
     "banner": {
-      "active": false,
+      "active": true,
       "idIOS": "ca-app-pub-8419654969814276/7291953690", 
       "idAndroid": "ca-app-pub-8419654969814276/6666047983",
     },
     "intersticial": {
-      "active": false,
+      "active": true,
       "idIOS": "ca-app-pub-8419654969814276/3520229645",
       "idAndroid": "ca-app-pub-8419654969814276/6721888307",
     },
